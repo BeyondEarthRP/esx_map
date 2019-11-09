299a7acf835f927c1fff8d91e278638b95bc4c5b
Twitchtitanium <57332409+Twitchtitanium@users.noreply.github.com>
2019-11-09 23:58:48 +0100

    Add files via upload

---
+++ esx_map/config.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+Config.Map = {
+  {name="~g~Quartier Families",color=24, id=378, x=-182.691, y=-1543.828, z=34.352},
+  {name="~y~Quartier Vagos",color=46, id=378,x=-1075.16, y =-1649.777, z= 4.501},
+  {name="~p~Quartier Ballas",color=27, id=378,x=107.616, y = -1940.67, z= 20.74},
+
+}
